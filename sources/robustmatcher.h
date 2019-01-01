86cd61f59679d5f4b7a76950f0d1678927aabd68
翔 陈 <cx3386@163.com>
2019-01-02 04:07:55 +0800

    1. 修正cleanDir 2. 修改config.ini 霍夫圆参数

--- sources/robustmatcher.h
+++ sources/robustmatcher.h
@@ -8,5 +8,10 @@ public:
 	~RobustMatcher();
 	bool match(cv::Mat src1, cv::Mat src2, cv::Mat &img_matches, double &angle);
 private:
+	void symmetryTest(
+		const std::vector<std::vector<cv::DMatch>> &matches1,
+		const std::vector<std::vector<cv::DMatch>> &matches2,
+		std::vector<cv::DMatch> &symMatches);
+	int ratioTest(std::vector<std::vector<cv::DMatch>> &matches);
 	cv::Mat getMask(cv::Size) const;
 };
